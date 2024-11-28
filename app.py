c8c6b1a73ca4c0620a94e044173367987191fb4b
kamila.olszowa <kamila.olszowa@hotmail.com>
2024-11-28 16:30:33 +0100

    add new todo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,16 +16,22 @@ class Todo(db.Model):
 with app.app_context():
     db.create_all()
 
-    new_todo = Todo(title="todo 1", complete=False)
-    # db.session.add(new_todo)
-    # db.session.commit()
-
 
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    if title and title.strip():
+        new_todo = Todo(title=title, complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for("index"))
 
 
 @app.route('/about')
